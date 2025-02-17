1cc112797a4c9b4d1096d9af06e03b33c029c700
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-02-17 18:48:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Data Science Junior')
 
 st.write('Hello Bro!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander("Data"):
+  st.write("X")
+  X_raw = df.drop('species', axis=1)
+  st.dataFrame(X_raw)
+
+  st.write("y")
+  y_raw = df.species
+  st.dataFrame(y_raw)
