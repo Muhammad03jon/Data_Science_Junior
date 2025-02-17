3034a84c1d90b44c7ba320b771bb0b06478e8fdb
Muhammad03jon <163680576+Muhammad03jon@users.noreply.github.com>
2025-02-17 19:36:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,30 @@ fig2 = px.histogram(
 )
 
 st.plotly_chart(fig2)
+
+input_df = pd.DataFrame(data, index=[0])
+input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+with st.expander("Input features"):
+  st.write("**Input Penguin**")
+  st.dataframe(input_df)
+  st.write("**Combined penguins data** (input row + original data)")
+  st.dataframe(input_penguins)
+
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, profix=encode)
+
+X = df_penguins[1:]
+input_row = df_penguins[:1]
+
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+def target_encoder(val):
+  return target_mapper[val]
+
+y = y_raw.apply(target_encoder)
+
+with st.expander('Data preparation'):
+  st.write("**Encoded X (input penguin)")
+  st.dataframe(input_row)
+  st.write("**Encoded y**")
+  st.write(y)
